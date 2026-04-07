6438727a87c1251be4ce532df6924d725a0916eb
Jonathan Monroe <yoda1304@gmail.com>
2026-04-06 22:39:12 -0400

    pinning versions and simplifying next

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ["res.cloudinary.com", "i.discogs.com", "st.discogs.com"],
-  },
+  // Temporarily disabled to test Vercel build
+  // images: {
+  //   domains: ["res.cloudinary.com", "i.discogs.com", "st.discogs.com"],
+  // },
 };
 
 export default nextConfig;
