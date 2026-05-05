b2ed5ebf360bd6a086e097cf88ce2cd7ea8c8f2c
Oditha <oditha14@gmail.com>
2026-05-04 21:49:02 -0400

    testing comment

--- src/app/components/ClientSidePages/discoveryClient/index.tsx
+++ src/app/components/ClientSidePages/discoveryClient/index.tsx
@@ -14,6 +14,7 @@ type DiscoverPageProps = {
 
 export default function DiscoverPage({}: DiscoverPageProps) {
   return (
+    // TEST!
     <div className="static origin-top flex flex-row gap-4 box-border bg-[#f3f3f3] items-center justify-center w-full lg:text-5xl md:text-4xl">
       {/* Left Sidebar */}
       <div className="w-1/5">
